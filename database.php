b707fe020240d073b93d4057ffb927f16af081c5
Yuvraj <Jindalyuvraj455@gmail.com>
2023-10-05 11:04:57 -0400

    updated file database.php

--- database.php
+++ database.php
@@ -3,17 +3,23 @@
   class Database{
     
     private $connection;
+    //construct method
     function __construct(){
     
       $this->connect_db();
     }
     
+    //connect with the database
     public function connect_db(){
       $this->connection = mysqli_connect('localhost', 'root', '', 'assignment1');
+
+      //if unable to connect to database outputs the connection faileed along with error message
       if(mysqli_connect_error()){
         die("Database Connection Failed" . mysqli_connect_error());
       }
     }
+
+    //create function for inserting the data received into the table or database created
     public function create($title, $fname, $lname, $email, $IDno, $mailingaddress, $phonenumber, $employmentstatus, $income, $accounttype, $usagepurpose){
       $sql = "INSERT INTO bankaccount (Title, fname, lname, Email, Government_ID, Mailing_Address, Phone_Number, Employment_Status, Annual_Income, Account_Type, Purpose) VALUES ('$title', '$fname', '$lname', '$email', '$IDno', '$mailingaddress', '$phonenumber', '$employmentstatus', '$income', '$accounttype', '$usagepurpose')";
       $res = mysqli_query($this->connection, $sql);
@@ -24,6 +30,7 @@
 			    return false;
 		  }
     }
+    
     public function read(){
 		    $sql = "SELECT * FROM bankaccount";
  		    $res = mysqli_query($this->connection, $sql);
